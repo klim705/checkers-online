68573ec9dd6384e50c3925e6b8af43cf800fb955
Katherina Lim <klim705@gmail.com>
2017-08-29 12:16:36 -0400

    Fix tests

--- src/checkerstest/CheckersMoveTest.java
+++ src/checkerstest/CheckersMoveTest.java
@@ -1,5 +1,8 @@
 package checkerstest;
 
+import checkers.BoardCell;
+import checkers.CheckersMove;
+import checkers.Piece;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,21 +14,21 @@ class CheckersMoveTest {
         assertFalse(checkersMove.isValid());
         assertFalse(checkersMove.isValidMove());
         assertFalse(checkersMove.isValidJump());
-        
-        BoardCell fromCell = new BoardCell(0, 0, BoardCell.GREEN);
-        fromCell.setPiece(new Piece(Piece.RED));
+
+        BoardCell fromCell = new BoardCell(0, 0, BoardCell.Color.GREEN);
+        fromCell.setPiece(new Piece(Piece.Color.RED));
         checkersMove.add(fromCell);
         assertFalse(checkersMove.isValid());
         assertFalse(checkersMove.isValidMove());
         assertFalse(checkersMove.isValidJump());
-        
-        BoardCell toCell = new BoardCell(1, 1, BoardCell.GREEN);
+
+        BoardCell toCell = new BoardCell(1, 1, BoardCell.Color.GREEN);
         toCell.setPiece(null);
         checkersMove.add(toCell);
         assertTrue(checkersMove.isValid());
         assertTrue(checkersMove.isValidMove());
         assertFalse(checkersMove.isValidJump());
-        
+
     }
 
     @Test
@@ -34,27 +37,27 @@ class CheckersMoveTest {
         assertFalse(checkersMove.isValid());
         assertFalse(checkersMove.isValidMove());
         assertFalse(checkersMove.isValidJump());
-        
-        BoardCell fromCell = new BoardCell(0, 0, BoardCell.GREEN);
-        fromCell.setPiece(new Piece(Piece.RED));
+
+        BoardCell fromCell = new BoardCell(0, 0, BoardCell.Color.GREEN);
+        fromCell.setPiece(new Piece(Piece.Color.RED));
         checkersMove.add(fromCell);
         assertFalse(checkersMove.isValid());
         assertFalse(checkersMove.isValidMove());
         assertFalse(checkersMove.isValidJump());
-        
-        BoardCell toCell = new BoardCell(2, 2, BoardCell.GREEN);
+
+        BoardCell toCell = new BoardCell(2, 2, BoardCell.Color.GREEN);
         toCell.setPiece(null);
         checkersMove.add(toCell);
-        assertFalse(checkersMove.isValid());
-        assertTrue(checkersMove.isValidMove());
-        assertFalse(checkersMove.isValidJump());
+        assertTrue(checkersMove.isValid());
+        assertFalse(checkersMove.isValidMove());
+        assertTrue(checkersMove.isValidJump());
     }
 
     @Test
     void add() {
         CheckersMove checkersMove = new CheckersMove();
         assertEquals(checkersMove.move.size(), 0);
-        BoardCell fromCell = new BoardCell(0, 0, BoardCell.GREEN);
+        BoardCell fromCell = new BoardCell(0, 0, BoardCell.Color.GREEN);
         checkersMove.add(fromCell);
         assertEquals(checkersMove.move.size(), 1);
         assertEquals(checkersMove.move.get(0), fromCell);
@@ -63,31 +66,31 @@ class CheckersMoveTest {
     @Test
     void get() {
         CheckersMove checkersMove = new CheckersMove();
-        BoardCell fromCell = new BoardCell(0, 0, BoardCell.GREEN);
-        BoardCell toCell = new BoardCell(1, 1, BoardCell.GREEN);
+        BoardCell fromCell = new BoardCell(0, 0, BoardCell.Color.GREEN);
+        BoardCell toCell = new BoardCell(1, 1, BoardCell.Color.GREEN);
         checkersMove.add(fromCell);
         checkersMove.add(toCell);
         assertEquals(checkersMove.get(0), fromCell);
-        assertEquals(checkersMove.get(0), toCell);
+        assertEquals(checkersMove.get(1), toCell);
     }
 
     @Test
     void removeCell() {
         CheckersMove checkersMove = new CheckersMove();
-        BoardCell fromCell = new BoardCell(0, 0, BoardCell.GREEN);
-        BoardCell toCell = new BoardCell(1, 1, BoardCell.GREEN);
+        BoardCell fromCell = new BoardCell(0, 0, BoardCell.Color.GREEN);
+        BoardCell toCell = new BoardCell(1, 1, BoardCell.Color.GREEN);
         checkersMove.add(fromCell);
         checkersMove.add(toCell);
         assertEquals(checkersMove.move.size(), 2);
-        checkersMove.remove(fromCell);
+        checkersMove.removeCell(fromCell);
         assertEquals(checkersMove.move.size(), 1);
     }
 
     @Test
     void size() {
         CheckersMove checkersMove = new CheckersMove();
-        BoardCell fromCell = new BoardCell(0, 0, BoardCell.GREEN);
-        BoardCell toCell = new BoardCell(1, 1, BoardCell.GREEN);
+        BoardCell fromCell = new BoardCell(0, 0, BoardCell.Color.GREEN);
+        BoardCell toCell = new BoardCell(1, 1, BoardCell.Color.GREEN);
         checkersMove.add(fromCell);
         assertEquals(checkersMove.size(), 1);
         checkersMove.add(toCell);
@@ -98,18 +101,18 @@ class CheckersMoveTest {
     void isEmpty() {
         CheckersMove checkersMove = new CheckersMove();
         assertTrue(checkersMove.isEmpty());
-        BoardCell fromCell = new BoardCell(0, 0, BoardCell.GREEN);
+        BoardCell fromCell = new BoardCell(0, 0, BoardCell.Color.GREEN);
         checkersMove.add(fromCell);
         assertFalse(checkersMove.isEmpty());
-        checkersMove.remove(fromCell);
+        checkersMove.removeCell(fromCell);
         assertTrue(checkersMove.isEmpty());
     }
 
     @Test
     void first() {
         CheckersMove checkersMove = new CheckersMove();
-        BoardCell fromCell = new BoardCell(0, 0, BoardCell.GREEN);
-        BoardCell toCell = new BoardCell(1, 1, BoardCell.GREEN);
+        BoardCell fromCell = new BoardCell(0, 0, BoardCell.Color.GREEN);
+        BoardCell toCell = new BoardCell(1, 1, BoardCell.Color.GREEN);
         checkersMove.add(fromCell);
         assertEquals(checkersMove.first(), fromCell);
         checkersMove.add(toCell);
@@ -119,8 +122,8 @@ class CheckersMoveTest {
     @Test
     void last() {
         CheckersMove checkersMove = new CheckersMove();
-        BoardCell fromCell = new BoardCell(0, 0, BoardCell.GREEN);
-        BoardCell toCell = new BoardCell(1, 1, BoardCell.GREEN);
+        BoardCell fromCell = new BoardCell(0, 0, BoardCell.Color.GREEN);
+        BoardCell toCell = new BoardCell(1, 1, BoardCell.Color.GREEN);
         checkersMove.add(fromCell);
         assertEquals(checkersMove.last(), fromCell);
         checkersMove.add(toCell);
@@ -130,8 +133,8 @@ class CheckersMoveTest {
     @Test
     void clear() {
         CheckersMove checkersMove = new CheckersMove();
-        BoardCell fromCell = new BoardCell(0, 0, BoardCell.GREEN);
-        BoardCell toCell = new BoardCell(1, 1, BoardCell.GREEN);
+        BoardCell fromCell = new BoardCell(0, 0, BoardCell.Color.GREEN);
+        BoardCell toCell = new BoardCell(1, 1, BoardCell.Color.GREEN);
         checkersMove.add(fromCell);
         checkersMove.add(toCell);
         assertFalse(checkersMove.move.isEmpty());
